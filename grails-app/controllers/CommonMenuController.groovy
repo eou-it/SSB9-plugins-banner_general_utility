a9044c540b3c57f892af4979b94e56f7241e8cd0
Deepak Nagaraj <Deepak.Nagaraj@ellucian.com>
2013-12-12 19:44:35 +0530

    Fixed Banner INB url issue

--- grails-app/controllers/CommonMenuController.groovy
+++ grails-app/controllers/CommonMenuController.groovy
@@ -417,11 +417,12 @@ class CommonMenuController {
 
     private def getBannerInbUrl(){
         String bannerInbUrl
+        def personalPref
 
         if(!session.getAttribute(BANNER_INB_URL)){
             if (!isSsbEnabled()){
-                bannerInbUrl = personalPreferenceService.fetchPersonalPreference(MAGELLAN,SERVER_DESIGNATION,INB)[0]
-                session.setAttribute(BANNER_INB_URL, bannerInbUrl.value)
+                personalPref = personalPreferenceService.fetchPersonalPreference(MAGELLAN,SERVER_DESIGNATION,INB)[0]
+                session.setAttribute(BANNER_INB_URL, personalPref.value)
             }
         }
         bannerInbUrl = session.getAttribute(BANNER_INB_URL)
