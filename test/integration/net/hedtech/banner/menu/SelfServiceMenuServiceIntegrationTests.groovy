aeb2f9094bbcf35e25310e63e379ef09d9a84d05
Michael Hitrik <michael.hitrik@ellucian.com>
2015-09-10 11:34:44 -0400

    GAF-1012 - Fixed failed tests

--- test/integration/net/hedtech/banner/menu/SelfServiceMenuServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/menu/SelfServiceMenuServiceIntegrationTests.groovy
@@ -1,12 +1,11 @@
 /*******************************************************************************
-Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
-*******************************************************************************/ 
+ Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.menu
 
 import groovy.sql.Sql
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 
 class SelfServiceMenuServiceIntegrationTests extends BaseIntegrationTestCase {
@@ -17,7 +16,6 @@ class SelfServiceMenuServiceIntegrationTests extends BaseIntegrationTestCase {
     public void setUp() {
         formContext = ['GUAGMNU']
         super.setUp()
-        dataSetup()
     }
 
     @Test
@@ -28,40 +26,15 @@ class SelfServiceMenuServiceIntegrationTests extends BaseIntegrationTestCase {
         sql.eachRow ("select spriden_pidm from spriden where spriden_id = 'HOSS001' and spriden_change_ind is not null") {
             pidm = it.spriden_pidm
         }
-        map = selfServiceMenuService.processMenu (null,null,pidm)
-        assert map.size() > 0
+        map = selfServiceMenuService.bannerMenu (null,null,pidm)
+        assert map?.size() > 0
     }
 
     @Test
     void testSelfServiceBannerMenu() {
         def map
-        map = selfServiceMenuService.processMenu (null,null,null)
+        map = selfServiceMenuService.bannerMenu (null,null,null)
         assert map.size() > 0
-    }
-
-    @Ignore /* Adding Ignore as the method combinedMenu does not exist in selfServiceMenuService*/
-    void testSelfServiceCombinedMenuMenu() {
-        def map
-        map = selfServiceMenuService.combinedMenu(null,null,null)
-        assert map.size() > 0
-    }
-
-    private def dataSetup() {
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-
-    }
-
-    @Ignore       /* Adding Ignore as the method searchMenuSSB does not exist in selfServiceMenuService*/
-    void testSelfServiceSearchMenu() {
-        def list
-        def pidm
-        def sql = new Sql(sessionFactory.getCurrentSession().connection())
-        sql.eachRow("select spriden_pidm from spriden where spriden_id = 'HOSS001' spriden_change_ind is not null") {
-            pidm = it.spriden_pidm
-        }
-
-        list = selfServiceMenuService.searchMenuSSB('Banner XE Registration',pidm)
-        assert list.size() > 0
     }
 
 }
