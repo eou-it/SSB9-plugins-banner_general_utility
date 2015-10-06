975fff67e9d6d9bd429aae8b8e6d865b17575583
kerickso <kyle.erickson@ellucian.com>
2015-10-06 10:56:53 -0600

    fixing datasource configuration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,15 +1,28 @@
+/** *******************************************************************************
+ Copyright 2009-2015 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************* */
+
+
+// Support Hibernate annotations
+import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
+
+
+// Note: Most of the dataSource configuration resides in resources.groovy and in the
+// installation-specific configuration file (see Config.groovy for the include).
+
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    configClass = GrailsAnnotationConfiguration.class
+    dialect = "org.hibernate.dialect.Oracle10gDialect"
+    loggingSql = false
+
 }
+
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
     config.location = [
-
             "classpath:hibernate-banner-general-utility.cfg.xml",
             "classpath:hibernate-banner-core.cfg.xml"
     ]
@@ -18,31 +31,14 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
         }
     }
 }
