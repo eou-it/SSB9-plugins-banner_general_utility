e5d415d5cc0ddb0eee1ad830937108e87eff18d3
MOHIT JAIN <mohit.jain@ellucian.com>
2016-06-09 15:53:10 +0530

    GAF-1337 copy right changes and test case added for the infinite menu issue

--- test/integration/net/hedtech/banner/menu/SelfServiceMenuServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/menu/SelfServiceMenuServiceIntegrationTests.groovy
@@ -53,6 +53,14 @@ class SelfServiceMenuServiceIntegrationTests extends BaseIntegrationTestCase {
     }
 
     @Test
+    void testSelfServiceBannerMenuWithMenuTrail() {
+        def map
+        map = selfServiceMenuService.bannerMenu (null,"Banner/Volunteer",null)
+
+        assertEquals "Banner/Volunteer", map.get(0).getMenu() ;
+    }
+
+    @Test
     void testSSLinks() {
 
         def ss = ["a","B"]
