567887505bd91f7bd2f896b3572f8b3191f59850
snehils <snehil.singh@ellucian.com>
2016-12-06 18:09:07 +0530

    HRU-7006: Integration tests added for About Service

---
+++ test/integration/net/hedtech/banner/about/AboutServiceIntegrationTests.groovy
@@ -0,0 +1,28 @@
+package net.hedtech.banner.about
+
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import java.lang.reflect.Method
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class AboutServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    def aboutService = new AboutService()
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+    }
+
+    @After
+    public void tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    public void getAboutsuccess(){
+        assertNotNull(aboutService.getAbout())
+    }
+}
