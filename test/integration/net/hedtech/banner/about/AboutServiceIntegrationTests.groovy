8d43acb797f54d621b9896b3f93716dd8225459e
Shaliya Khwaja <shaliya.khwaja@ellucian.com>
2017-04-03 13:00:35 +0530

    HRU-7166 - Added tests for improving coverage of AboutService

--- test/integration/net/hedtech/banner/about/AboutServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/about/AboutServiceIntegrationTests.groovy
@@ -1,17 +1,15 @@
 /*******************************************************************************
- Copyright 2016 Ellucian Company L.P. and its affiliates.
+ Copyright 2016-2017 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
 package net.hedtech.banner.about
 
 import net.hedtech.banner.testing.BaseIntegrationTestCase
-import java.lang.reflect.Method
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class AboutServiceIntegrationTests extends BaseIntegrationTestCase {
-
     def aboutService = new AboutService()
 
     @Before
@@ -27,6 +25,57 @@ class AboutServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     public void getAboutSuccess(){
+        def releasePropertiesFile = new File( "target/classes/release.properties" )
         assertNotNull(aboutService.getAbout())
+        releasePropertiesFile.delete()
+    }
+
+    @Test
+    public void testGetApplicationName() {
+        assertNotNull(aboutService.getApplicationName())
+    }
+
+    @Test
+    public void testGetCopyright() {
+        assertNotNull(aboutService.getCopyright())
+    }
+
+    @Test
+    public void testGetCopyrightLegalNotice() {
+        assertNotNull(aboutService.getCopyrightLegalNotice())
+    }
+
+    @Test
+    public void testGetUserName() {
+        assertNotNull(aboutService.getUserName())
+    }
+
+    @Test
+    public void testformatCamelCaseToEnglish() {
+        def value = 'platformSandbox'
+        assertNotNull(aboutService.formatCamelCaseToEnglish(value))
+        assertNull(aboutService.formatCamelCaseToEnglish())
+
+    }
+
+    @Test
+    public void testGetMepDescription() {
+        assertNull(aboutService.getMepDescription())
+    }
+
+    @Test
+    public void testGetAppInfo() {
+        assertNotNull(aboutService.getAppInfo())
+    }
+    @Test
+    public void testGetDbInstanceName() {
+        assertNotNull(aboutService.getDbInstanceName())
+    }
+
+
+    @Test
+    public void testGetPluginsInfo() {
+        def pattern
+        assertNotNull(aboutService.getPluginsInfo(pattern))
     }
 }
