15da7721fe7ae789f3de2ba18085a637b431320a
Shaliya Khwaja <shaliya.khwaja@ellucian.com>
2017-04-05 14:56:41 +0530

    HRU-7166-Updated to read assertion value from messages.properties

--- test/integration/net/hedtech/banner/about/AboutServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/about/AboutServiceIntegrationTests.groovy
@@ -4,6 +4,7 @@
 
 package net.hedtech.banner.about
 
+import net.hedtech.banner.i18n.MessageHelper
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
@@ -25,10 +26,10 @@ class AboutServiceIntegrationTests extends BaseIntegrationTestCase {
 
     @Test
     public void getAboutSuccess(){
-        assertEquals("About Banner",aboutService.getAbout().get("api.title"))
+        assertEquals(MessageHelper.message("about.banner.title"),aboutService.getAbout().get("api.title"))
         assertEquals("banner_general_utility",aboutService.getAbout().get("about.banner.application.name"))
         assertTrue(aboutService.getAbout().get("about.banner.application.version").toString().contains("Version"))
-        assertEquals("2017 Ellucian Company L.P. and its affiliates. All rights reserved.",aboutService.getAbout().get("about.banner.copyright"))
-        assertTrue(aboutService.getAbout().get("about.banner.copyrightLegalNotice").toString().contains("This software contains confidential and proprietary information of Ellucian or its subsidiaries"))
+        assertEquals(MessageHelper.message("net.hedtech.banner.login.copyright1"),aboutService.getAbout().get("about.banner.copyright"))
+        assertEquals(MessageHelper.message("net.hedtech.banner.login.copyright2"),aboutService.getAbout().get("about.banner.copyrightLegalNotice"))
     }
 }
