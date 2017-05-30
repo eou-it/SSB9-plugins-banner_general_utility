5e2d54a9ccf2a7d305183241f6947cd5dd2b6604
snehils <snehil.singh@ellucian.com>
2017-05-30 20:05:57 +0530

    EXTZ-1632: Code coverage increased to over 80% and refactored code in TextManagerDB.groovy and TextManagerService.groovy

---
+++ grails-app/services/net/hedtech/banner/textmanager/TextManagerService.groovy
@@ -0,0 +1,319 @@
+/******************************************************************************
+ *  Copyright 2017 Ellucian Company L.P. and its affiliates.                  *
+ ******************************************************************************/
+
+package net.hedtech.banner.textmanager
+
+import grails.util.Holders
+import groovy.sql.Sql
+import org.apache.log4j.Logger
+
+class TextManagerService {
+    def sessionFactory
+
+    static transactional = false //Transaction not managed by hibernate
+
+    private final static Logger log = Logger.getLogger(TextManagerService.class.name)
+    static final String ROOT_LOCALE_APP = 'en' // This will be the locale assumed for properties without locale
+    // Save the chosen source language as root (as user cannot change translation)
+    static final String PROJECT_CFG_KEY_APP = 'BAN_APP'
+    final def ROOT_LOCALE_TM   = 'root'
+
+    private def tranManProjectCache
+    private def cacheTime
+    private Boolean tmEnabled = true
+
+    public static dbValues = [:]
+
+    private def tranManProject() {
+        if (!tmEnabled) {
+            return
+        }
+        if (cacheTime && (new Date().getTime() - cacheTime.getTime()) < 5 * 60 * 1000) {
+            return tranManProjectCache
+        }
+        Sql sql = new Sql(sessionFactory.getCurrentSession().connection())
+        String appName = Holders.grailsApplication.metadata['app.name']
+        String result = ""
+        int matches = 0
+        try {
+            // Find projects with a matching application name in GMRPCFG
+            // If more matches exist pick the project with the latest activity date
+            def statement = """
+                               select GMRPCFG_PROJECT from GMRPCFG join GMBPROJ on GMBPROJ_PROJECT=GMRPCFG_PROJECT
+                               where GMRPCFG_KEY = $PROJECT_CFG_KEY_APP and GMRPCFG_VALUE = $appName
+                               order by GMRPCFG_ACTIVITY_DATE
+                            """
+            sql.eachRow(statement) { row ->
+                result = row.GMRPCFG_PROJECT
+                matches++
+            }
+        } catch (e) {
+            log.error("Error initializing text manager", e)
+            tmEnabled = false
+        } finally {
+            sql?.close()
+        }
+        if (matches > 1) {
+            log.warn "Multiple Text Manager projects are configured for application $appName. Using ${result}. It is recommended to correct this."
+        }
+        if (matches == 0) {
+            log.warn "No Text Manager project is configured for application $appName. Text Manager customization is not possible."
+        }
+        tranManProjectCache = result
+        cacheTime = new Date()
+        log.debug "Using Text Manager project $result"
+        result
+    }
+
+    def createProjectForApp(projectCode, projectDescription) {
+        if (!tmEnabled) {
+            return
+        }
+        if (!tranManProject()) {
+            Sql sql = new Sql(sessionFactory.getCurrentSession().connection())
+            def appName = Holders.grailsApplication.metadata['app.name']
+            try {
+                def statement = """
+                                   insert into GMBPROJ (GMBPROJ_PROJECT, GMBPROJ_ACTIVITY_DATE, GMBPROJ_DESC,
+                                   GMBPROJ_OWNER,GMBPROJ_USER_ID) values ($projectCode, sysdate, $projectDescription,
+                                   'TRANMGR','ban_ss_user')
+                                """
+                sql.execute(statement)
+                statement = """
+                               insert into GMRPCFG (GMRPCFG_PROJECT, GMRPCFG_KEY, GMRPCFG_VALUE, GMRPCFG_DESC,
+                               GMRPCFG_USER_ID,GMRPCFG_ACTIVITY_DATE) values ($projectCode, $PROJECT_CFG_KEY_APP,
+                               $appName, 'Banner Application in this project','ban_ss_user',sysdate )
+                            """
+                sql.execute(statement)
+                cacheTime = null
+                log.info "Created TranMan project $projectCode"
+            } finally {
+                sql?.close()
+            }
+        }
+    }
+
+    //Used to clean test project
+    def deleteProjectforApp() {
+        if (!tmEnabled) {
+            return
+        }
+        def project = tranManProject()
+        if (project) {
+            Sql sql = new Sql(sessionFactory.getCurrentSession().connection())
+            try {
+                def statement = """
+                                   begin
+                                    delete from GMRPCFG where GMRPCFG_project=$project;
+                                    delete from GMRSPRP where GMRSPRP_project=$project;
+                                    delete from GMRSHST where GMRSHST_project=$project;
+                                    delete from GMRPOBJ where GMRPOBJ_project=$project;
+                                    delete from GMBPROJ where GMBPROJ_project=$project;
+                                   end;
+                                """
+                sql.execute(statement)
+                cacheTime = null
+                log.info "Deleted TranMan project $project"
+            } catch(e){
+                log.error("Failed in deleting the project", e)
+            } finally {
+                sql?.close()
+            }
+        }
+    }
+
+
+        def save(properties, name, srcLocale = ROOT_LOCALE_APP, locale) {
+        if (!tmEnabled) {
+            return
+        }
+        def project = tranManProject()
+        if (project) {
+            def textManagerDB = new TextManagerDB()
+            textManagerDB.createConnection()
+            int cnt = 0
+            try {
+                String msg = """
+                                Arguments: mo=<mode> ba=<batch> lo=<db logon> pc=<TranMan Project> sl=<source language>
+                                tl=<target language>  sf=<source file> tf=<target file>
+                                mode: s (extract) | r (reverse extract) | t (translate) | q (quick translate - no check)
+                                batch: [y|n]. n (No) is default. If y (Yes), the module record will be updated with
+                                file locations etc.
+                             """
+                dbValues.projectCode = project
+                dbValues.moduleName =  name.toUpperCase()
+                dbValues.srcLocale =  ROOT_LOCALE_TM
+                dbValues.srcFile = locale == ROOT_LOCALE_APP ? "${name}.properties" : "${name}_${locale}.properties"
+                dbValues.srcIndicator = locale == srcLocale ? 's' : 'r'
+                dbValues.tgtLocale = locale == srcLocale ? '' : "${locale.replace('_','')}"
+
+                if (dbValues.srcIndicator == null) {
+                    dbValues << [srcIndicator:"s"]
+                } else if (dbValues.srcIndicator.equals("t")) {
+                    if (dbValues.tgtFile == null) {
+                        log.error "No target file specified (tgtFile=...) \n" + msg
+                    }
+                    if (dbValues.tgtLocale == null) {
+                        log.error "No target language specified (tgtLocale=...) \n" + msg
+                    }
+                } else if (dbValues.srcIndicator.equals("r")) {
+                    if (dbValues.tgtLocale == null) {
+                        log.error "No target language specified (tgtLocale=...) \n" + msg
+                    }
+                }
+
+                textManagerDB.setDBContext(dbValues)
+                textManagerDB.setDefaultProp(dbValues)
+                def defaultObjectProp = textManagerDB.getDefaultObjectProp()
+                final String sep = "."
+                int sepLoc
+
+                properties.each { property ->
+                    sepLoc = 0
+                    String key = property.key
+                    String value = property.value
+                    sepLoc = key.lastIndexOf(sep)
+                    if (sepLoc == -1) {
+                        sepLoc = 0
+                    }
+                    defaultObjectProp.parentName = sep + key.substring(0, sepLoc) //. plus expression between brackets in [x.y...].z
+                    defaultObjectProp.objectName = key.substring(sepLoc)       // expression between brackets in x.y....[z]
+                    defaultObjectProp.string = smartQuotesReplace(value)
+                    log.info key + " = " + defaultObjectProp.string
+                    textManagerDB.setPropString(defaultObjectProp)
+                    cnt++
+                }
+                //Invalidate strings that are in db but not in property file
+                if (dbValues.srcIndicator.equals("s")) {
+                    textManagerDB.invalidateStrings()
+                }
+                textManagerDB.setModuleRecord(dbValues)
+
+            } catch (e){
+                log.error("Exception in saving properties", e)
+            }finally{
+                textManagerDB.closeConnection()
+            }
+            return [error: null, count: cnt]
+        }
+        return [error: "Unable to save - no Project configured", count: 0]
+    }
+
+    def cacheMsg = [:]
+    def localeLoaded = [:]
+    def timeOut = 60 * 1000 as long //milli seconds
+
+    def findMessage(key, locale) {
+        if (!tmEnabled) {
+            return null
+        }
+        def msg
+        def t0 = new Date()
+        def cacheAgeMilis = t0.getTime() - (localeLoaded[locale]?.getTime()?: 0L)
+        if (cacheAgeMilis < timeOut) {
+            msg = cacheMsg[key] ? cacheMsg[key][locale] : null
+        } else {
+            def tmLocale = locale?.toString().replace('_', '')
+            def tmProject = tranManProject()
+            if (!tmEnabled) {
+                return null
+            }
+            // Add timeOut to the cacheAge to be sure no updates in TextManager are missed
+            def params = [locale: tmLocale, pc: tmProject, days_ago: (cacheAgeMilis+timeOut)/1000/24/3600 , max_distance: 1]
+            //max_distance: 1 means use strings with a matching locale, do not use a string from a different territory
+            //max_distance: 2 means also use a string from a different territory (but just picks one if multiple territories exist)
+            Sql sql = new Sql(sessionFactory.getCurrentSession().connection())
+            sql.cacheStatements = false
+            //Query fetching changed messages. Don't use message with status pending (11).
+            def statement = """
+                    |WITH locales AS
+                    |( 
+                    |  SELECT * FROM (
+                    |     SELECT ( 1 + DECODE(gtvlang_code,:locale,0,1) -INSTR(:locale,gtvlang_code)) AS distance
+                    |     ,gtvlang_code AS lang_code 
+                    |     FROM gtvlang
+                    |     WHERE gtvlang_code LIKE SUBSTR(:locale,1,2)||'%'
+                    |  ) 
+                    |  WHERE distance <=:max_distance
+                    |),
+                    |props_with_changes AS 
+                    |(
+                    |  SELECT DISTINCT 
+                    |  gmrsprp_project,gmrsprp_module_name, gmrsprp_module_type, gmrsprp_parent_name, gmrsprp_parent_type, 
+                    |  gmrsprp_object_name, gmrsprp_object_type, gmrsprp_object_prop, locales.lang_code
+                    |  FROM gmrsprp, locales /*Carthesian Join with locales*/
+                    |  WHERE gmrsprp_project = :pc 
+                    |    AND gmrsprp_activity_date >= (SYSDATE - :days_ago)
+                    |    AND gmrsprp_lang_code LIKE  SUBSTR(:locale,1,2)||'%'
+                    |)
+                    |SELECT 
+                    |/*locales.code, locales.distance, gmrsprp_module_name AS source_name,*/
+                    |SUBSTR(gmrsprp_parent_name,2)||gmrsprp_object_name AS key
+                    |,DECODE(gmrsprp_stat_code,11,'',gmrsprp_pre_str||gmbstrg_string||gmrsprp_pst_str) AS string
+                    |FROM locales, gmrsprp p1, gmbstrg
+                    |WHERE gmrsprp_project = :pc
+                    |AND gmrsprp_module_type = 'J'
+                    |AND (gmrsprp_project, gmrsprp_module_name, gmrsprp_module_type, gmrsprp_parent_name, gmrsprp_parent_type,
+                    |     gmrsprp_object_name, gmrsprp_object_type, gmrsprp_object_prop, gmrsprp_lang_code)
+                    | IN (SELECT * FROM props_with_changes)
+                    |--    
+                    |AND gmrsprp_lang_code = locales.lang_code
+                    |AND gmbstrg_strcode=gmrsprp_strcode
+                    |AND locales.distance = (
+                    |   SELECT
+                    |   MIN( locales.distance ) 
+                    |   FROM locales, gmrsprp 
+                    |   WHERE gmrsprp_lang_code=locales.lang_code 
+                    |     AND (gmrsprp_project, gmrsprp_module_name, gmrsprp_module_type, gmrsprp_parent_name, gmrsprp_parent_type,
+                    |          gmrsprp_object_name, gmrsprp_object_type, gmrsprp_object_prop, gmrsprp_lang_code)
+                    |      IN (SELECT * FROM props_with_changes)
+                    |) 
+                    |""".stripMargin()
+
+            //and parent_type = 10 and parent_name = :pn and object_type = 26 and object_name = :on and object_prop = 438
+            def rows
+            try {
+                rows = sql.rows(statement, params, null)
+            }
+            catch (e) {
+                log.error("Exception in finding message for key=$key, locale=$locale", e)
+            }
+            finally {
+                sql?.close()
+            }
+            def t1 = new Date()
+            if (rows.size()) {
+                rows.each { row ->
+                    def translations = cacheMsg[row.key] ?: [:]
+                    translations[locale] = row.string
+                    cacheMsg[row.key] = translations
+                }
+            }
+            localeLoaded[locale] = t0
+            msg = cacheMsg[key] ? cacheMsg[key][locale] : null
+            def t2 = new Date()
+            log.debug "Reloaded ${rows.size()} modified texts in ${t2.getTime() - t0.getTime()} ms . Query+Fetch time: ${t1.getTime() - t0.getTime()}"
+        }
+        msg
+    }
+
+    String smartQuotesReplace(String s) {
+        StringBuffer res = new StringBuffer()
+        char c
+        s.eachWithIndex{ item, index ->
+            c = item
+            if (c == '\'') {
+                // look ahead
+                if (index + 1 < s.length() && s[index + 1] == '\'') {
+                    res.append(c)
+                } else {
+                    res.append("\u2019")
+                }
+            } else {
+                res.append(c)
+            }
+        }
+        return res.toString()
+    }
+}
---
+++ src/groovy/net/hedtech/banner/textmanager/TextManagerDB.groovy
@@ -0,0 +1,212 @@
+/******************************************************************************
+ *  Copyright 2017 Ellucian Company L.P. and its affiliates.                  *
+ ******************************************************************************/
+package net.hedtech.banner.textmanager
+
+import grails.util.Holders as CH
+import groovy.sql.Sql
+import org.apache.log4j.Logger
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+import java.sql.SQLException
+
+class TextManagerDB {
+    def dataSource
+    Sql sql
+    ObjectProperty defaultProp
+
+    String projectCode
+    String langCodeSrc
+    String langCodeTgt
+    String moduleType
+    String moduleName
+
+    private static final def log = Logger.getLogger(TextManagerDB.class.name)
+
+    static class ObjectProperty {
+        String langCode
+        int parentType
+        String parentName
+        int objectType
+        String objectName
+        int propCode
+        int status
+        String string
+        int match
+        String statusX
+
+        public ObjectProperty() {
+            parentType = 10  //Module
+            objectType = 26  //Property Class
+            propCode = 438  //D2FP_TEXT
+            status = 0
+        }
+    }
+
+    public createConnection (){
+        def ctx = CH.servletContext.getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)
+        def sessionFactory = ctx.sessionFactory
+        sql = new Sql(sessionFactory.getCurrentSession().connection())
+    }
+
+    public void closeConnection() throws SQLException {
+        if ( !sql) {
+            sql.commit();
+            sql.close();
+        }
+    }
+
+    public ObjectProperty getDefaultObjectProp() {
+        return defaultProp
+    }
+
+    void setDefaultProp(Map dbValues){
+        defaultProp = new ObjectProperty()
+        if(dbValues.srcIndicator.equals("s"))
+            defaultProp.langCode = dbValues.srcLocale
+        else
+            defaultProp.langCode = dbValues.tgtLocale
+    }
+
+    void setDBContext(Map dbValues) throws SQLException {
+        int defStatus = 1 //set to 1 for properties - assume translatable by default
+        int sqlTrace = 0
+        long timestamp = System.currentTimeMillis()
+        projectCode   = dbValues.projectCode
+        langCodeSrc  = dbValues.srcLocale
+        langCodeTgt  = dbValues.tgtLocale
+        moduleType = 'J'
+        moduleName = dbValues.moduleName
+
+        //Reverse extract.
+        if (dbValues.srcIndicator.equals("r")) {
+            defStatus = 7 //set to Reverse extracted
+        }
+        try {
+            def params = [sqlTrace, projectCode, langCodeSrc, langCodeTgt, moduleType, moduleName, defStatus]
+            def stmt = """
+                          Begin
+                            if :1>0 then
+                                DBMS_SESSION.SET_SQL_TRACE(TRUE);
+                            end if;
+                            GMKOBJI.P_SETCONTEXT(:2, :3, :4, :5, :6, :7);
+                          End;
+                       """
+            sql.call(stmt, params)
+        } catch (SQLException e) {
+            log.error("Error in SetDBContext", e)
+        }
+        timestamp = System.currentTimeMillis() - timestamp
+        log.debug("SetDBContext done in " + timestamp + " ms")
+    }
+
+
+    void setModuleRecord(Map dbValues) throws SQLException {
+        String dataSrc = dbValues.srcFile
+        String langCode = langCodeSrc
+        String modDesc
+
+
+        switch (dbValues.srcIndicator.charAt(0) ) {
+            case 's':
+                dataSrc = dbValues.srcFile
+                langCode = langCodeSrc
+                modDesc = "Properties batch extract"
+                break
+            case 'r':
+                dataSrc = dbValues.srcFile
+                langCode = langCodeTgt
+                modDesc = "Properties batch reverse extract"
+                break
+            default: //q and t both translate
+                dataSrc = dbValues.tgtFile
+                langCode = langCodeTgt
+                modDesc = "Properties batch translate"
+        }
+        try{
+            def params = [projectCode, moduleName, moduleType, langCode, langCodeSrc, modDesc, dataSrc]
+            def stmt = """
+                          Declare
+                            b1 GMRMDUL.GMRMDUL_PROJECT%type :=:1;
+                            b2 GMRMDUL.GMRMDUL_MODULE_NAME%type  :=:2;
+                            b3 GMRMDUL.GMRMDUL_MODULE_TYPE%type  :=:3;
+                            b4 GMRMDUL.GMRMDUL_LANG_CODE%type    :=:4;
+                            b5 GMRMDUL.GMRMDUL_SRC_LANG_CODE%type:=:5;
+                            b6 GMRMDUL.GMRMDUL_MOD_DESC%type     :=:6;
+                            b7 GMRMDUL.GMRMDUL_DATASOURCE%type   :=:7;
+                          Begin
+                            insert into
+                            GMRMDUL (GMRMDUL_PROJECT,GMRMDUL_MODULE_NAME,GMRMDUL_MODULE_TYPE,
+                                     GMRMDUL_LANG_CODE,GMRMDUL_SRC_LANG_CODE,GMRMDUL_MOD_DESC,GMRMDUL_DATASOURCE,
+                                     GMRMDUL_USER_ID,GMRMDUL_ACTIVITY_DATE
+                                    )
+                            values (b1,b2,b3,b4,b5,b6,b7,user,sysdate);
+                            Exception when dup_val_on_index then
+                            update GMRMDUL
+                            set GMRMDUL_ACTIVITY_DATE = sysdate,
+                                GMRMDUL_MOD_DESC  = b6,
+                                GMRMDUL_DATASOURCE= b7,
+                                GMRMDUL_USER_ID   = user
+                            where GMRMDUL_PROJECT = b1 and
+                                  GMRMDUL_MODULE_NAME  = b2 and
+                                  GMRMDUL_MODULE_TYPE  = b3 and
+                                  GMRMDUL_LANG_CODE    = b4;
+                          End;
+                       """
+            sql.call(stmt, params)
+            log.debug "setModuleRecord project: $projectCode, sourceName: $moduleName, langCode: $langCode, langCodeSrc: $langCodeSrc, modDesc: $modDesc, dataSource: $dataSrc"
+        } catch (SQLException e) {
+            log.error("Error in setModuleRecord", e)
+        }
+    }
+
+    void setPropString(ObjectProperty op) throws SQLException {
+        try {
+            def params = [Sql.VARCHAR, op.langCode, op.parentType, op.parentName, op.objectType, op.objectName, op.propCode, Sql.inout(Sql.INTEGER(op.status)), op.string]
+            def stmt = """
+                          Begin
+                            :1  := GMKOBJI.F_SETPROPSTRINGX(
+                                    pLang_Code    => :2,
+                                    pParent_type  => :3,
+                                    pParent_name  => :4,
+                                    pObject_type  => :5,
+                                    pObject_name  => :6,
+                                    pObject_prop  => :7,
+                                    pTransl_stat  => :8,
+                                    pProp_string  => :9
+                                   );
+                          End;
+                       """
+            sql.call(stmt, params){statusX, status ->
+                op.statusX = statusX
+                op.status = status
+                log.debug "setPropString $op.statusX Text: $op.string"
+            }
+        } catch (SQLException e) {
+                log.error("Error in setPropString", e)
+        }
+    }
+
+    void invalidateStrings() throws SQLException {
+        long timestamp = System.currentTimeMillis()
+        try {
+            def params = [projectCode, langCodeSrc,moduleType, moduleName, Sql.INTEGER]
+            def stmt = """
+                          Begin
+                            update GMRSPRP set GMRSPRP_STAT_CODE=-5
+                            where GMRSPRP_PROJECT=:1
+                                and GMRSPRP_LANG_CODE   =:2
+                                and GMRSPRP_MODULE_TYPE =:3
+                                and GMRSPRP_MODULE_NAME =:4
+                                and GMRSPRP_ACTIVITY_DATE<GMKOBJI.g_session_time;
+                            :5:=GMKOBJI.f_CleanUp(-5);
+                          End;
+                       """
+            sql.call(stmt, params){
+                timestamp = System.currentTimeMillis() - timestamp
+                log.debug "Obsoleted " + it + " properties in " + timestamp + " ms"
+            }
+        } catch (SQLException e) {
+            log.error("Error in dbif.invalidateStrings", e)
+        }
+    }
+}
