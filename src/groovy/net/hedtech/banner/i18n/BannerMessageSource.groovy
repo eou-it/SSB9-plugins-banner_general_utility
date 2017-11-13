02d38681ed1c51d88531cf34aeaa0231fc27f7d1
Shaliya Khwaja <shaliya.khwaja@ellucian.com>
2017-11-13 12:37:26 +0530

    EXTZ-2195- Added bean creation of textManagerService

--- src/groovy/net/hedtech/banner/i18n/BannerMessageSource.groovy
+++ src/groovy/net/hedtech/banner/i18n/BannerMessageSource.groovy
@@ -212,6 +212,9 @@ class BannerMessageSource extends PluginAwareResourceBundleMessageSource {
      */
     public void mergeTextManagerProperties( Locale locale, Properties props ) {
         Map entries = new LinkedHashMap()
+        if (!textManagerService) {
+            textManagerService = ServletContextHolder.getServletContext()?.getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)?.getBean("textManagerService")
+        }
         props.each  { key, _ ->
             String value = textManagerService.findMessage(key, locale)
             if ( value != null ) {
