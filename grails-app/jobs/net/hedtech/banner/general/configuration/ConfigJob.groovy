c393178953fc3e25a9483bf383c1a6598de10d01
Barun Kumar Patel <barun.patel@ellucian.com>
2018-07-05 08:17:40 +0530

    HRU-7919 :- Added changes as the ConfigJob was not considering the Config parameter.

--- grails-app/jobs/net/hedtech/banner/general/configuration/ConfigJob.groovy
+++ grails-app/jobs/net/hedtech/banner/general/configuration/ConfigJob.groovy
@@ -3,7 +3,6 @@
  *******************************************************************************/
 package net.hedtech.banner.general.configuration
 
-import org.apache.log4j.Logger
 import grails.util.Holders as CH
 import org.springframework.dao.InvalidDataAccessResourceUsageException
 import groovy.util.logging.Slf4j
@@ -14,18 +13,22 @@ class ConfigJob {
     def configPropertiesService
     def springSecurityService
 
-    static def delay = CH.config.configJob?.delay instanceof Integer? CH.config.configJob?.delay : 60000
-    static def interval = CH.config.configJob?.interval instanceof Integer? CH.config.configJob?.interval : 60000
-    def concurrent = false
-    static def actualCount = CH.config.configJob?.actualCount instanceof Integer? CH.config.configJob?.actualCount > 0 ? CH.config.configJob?.actualCount -1 : CH.config.configJob?.actualCount : -1
+    // TODO :grails_332_change, needs to revisit
+    Boolean concurrent = false
+    static Integer actualCount = 0
+    //static def actualCount = CH.config.configJob?.actualCount instanceof Integer? CH.config.configJob?.actualCount > 0 ? CH.config.configJob?.actualCount -1 : CH.config.configJob?.actualCount : -1
 
     static triggers = {
+        Integer delay = CH.config.configJob?.delay instanceof Integer? CH.config.configJob?.delay : 60000
+        Integer interval = CH.config.configJob?.interval instanceof Integer? CH.config.configJob?.interval : 60000
+        actualCount = CH.config.configJob?.actualCount instanceof Integer? CH.config.configJob?.actualCount > 0 ? CH.config.configJob?.actualCount -1 : CH.config.configJob?.actualCount : -1
+
         simple startDelay: delay, repeatInterval: interval, repeatCount : actualCount // execute job once in 15 minutes
     }
 
     def execute() {
-
-        log.info("Running Config Job with configurations actualCount =  ${actualCount}, delay = ${delay}, repeatInterval= ${interval}")
+        println "CH.config.configJob?.actualCount = ${CH.config.configJob?.actualCount}"
+        log.info("Running Config Job with configurations actualCount =  ${actualCount}")
         if (actualCount != 0) {
             try {
                 configPropertiesService.setConfigFromDb()
