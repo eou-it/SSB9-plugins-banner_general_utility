9303c5e3285fae0eaa3855eaf0da7e67f59facc8
Barun Kumar Patel <barun.patel@ellucian.com>
2018-08-28 08:56:46 +0530

    HRU-8028 :- Fixed the Keyboard shortcut display issue with Tomcat and added fix for 9.28.2 release.

--- grails-app/controllers/net/hedtech/banner/general/configuration/ShortcutController.groovy
+++ grails-app/controllers/net/hedtech/banner/general/configuration/ShortcutController.groovy
@@ -5,6 +5,7 @@ package net.hedtech.banner.general.configuration
 
 import grails.converters.JSON
 import grails.util.Environment
+import groovy.io.FileVisitResult
 import groovy.json.JsonBuilder
 import grails.core.GrailsApplication
 import org.springframework.context.i18n.LocaleContextHolder
@@ -14,16 +15,16 @@ import static groovy.io.FileType.FILES
 class ShortcutController {
     GrailsApplication grailsApplication
     private static List<File> JSONFILELIST = []
-    private static JsonBuilder OUTPUTJSON
     def messageSource
 
     def data() {
-        if (JSONFILELIST.isEmpty() || OUTPUTJSON == null) {
+        LinkedHashMap shortcutKeysMap = new LinkedHashMap()
+        if (JSONFILELIST.isEmpty()) {
             JSONFILELIST = getShortcutJSONFiles()
-            LinkedHashMap shortcutKeys = populateOSSpecificShortcutFromJsonFiles()
-            OUTPUTJSON = getMessageForShortcutKeys(shortcutKeys)
         }
-        render OUTPUTJSON
+        shortcutKeysMap = populateOSSpecificShortcutFromJsonFiles()
+        JsonBuilder outputJson = getMessageForShortcutKeys(shortcutKeysMap)
+        render outputJson
     }
 
 
@@ -35,29 +36,41 @@ class ShortcutController {
             dirPath = System.properties['user.dir']
         }
         List<File> jsonFiles = getJSONFilesFromDirectory(dirPath)
+        log.debug("{} shortcut JSON Files found.", jsonFiles?.size())
+        log.debug("Json Files = {}", jsonFiles)
         return jsonFiles
     }
 
 
     private static List<File> getJSONFilesFromDirectory(String dirPath){
+        final excludedDirs = ['.git', 'gradle', '.idea', 'node_modules', '.gradle', 'build']
         List<File> jsonFiles = []
-        new File(dirPath).eachFileRecurse(FILES) {
+       /* new File(dirPath).eachFileRecurse(FILES) {
             if(it.name.endsWith('shortcut_properties.json')) {
                 jsonFiles << it
             }
-        }
+        }*/
+
+        new File(dirPath).traverse(
+                type                : FILES,
+                preDir              : { if (it.name in excludedDirs) return FileVisitResult.SKIP_SUBTREE }, // excludes children of excluded dirs
+                excludeNameFilter   : { it in excludedDirs }, // excludes the excluded dirs as well
+                nameFilter          :  ~/.*shortcut_properties.json/, //{ it.name?.endsWith('shortcut_properties.json')} // matched only given names
+                //nameFilter          :  ~/.*shortcut_properties.*\.json/, //{ it.name?.endsWith('shortcut_properties.json')} // matched only given names
+        ) {jsonFiles << it }
+        
         return jsonFiles
     }
 
 
     private static LinkedHashMap populateOSSpecificShortcutFromJsonFiles() {
-        List jsonData = []
+        def jsonData
         List windowsList = []
         List macList = []
         JSONFILELIST?.each { jsonFile ->
-            jsonData << JSON.parse(jsonFile.text)
-            windowsList << jsonData.windows
-            macList << jsonData.mac
+            jsonData = JSON.parse(jsonFile.text)
+            windowsList << jsonData?.windows
+            macList << jsonData?.mac
         }
         LinkedHashMap shortcutKeys = new LinkedHashMap()
         shortcutKeys << [windows: windowsList]
@@ -73,31 +86,41 @@ class ShortcutController {
 
         shortcutKeys.windows.each { additionalWindowsList ->
             additionalWindowsList.each{ sectionWindowsHeading ->
-                        List tempList
-                        sectionWindowsHeading.each { windowsShortcuts ->
-                            tempList = new ArrayList()
-                            windowsShortcuts.value.each { windowsShortcut ->
-                                windowsShortcut.combination = getMessage(windowsShortcut.combination)
-                                windowsShortcut.description = getMessage(windowsShortcut.description)
-                                tempList.add(windowsShortcut)
-                            }
-                            sectionHeadingWindowsMap.put(getMessage(windowsShortcuts.key), tempList)
-                        }
-
+                List tempList
+                sectionWindowsHeading.each { windowsShortcuts ->
+                    tempList = new ArrayList()
+                    windowsShortcuts.value.each { windowsShortcut ->
+                        windowsShortcut.combination = getMessage(windowsShortcut.combination)
+                        windowsShortcut.description = getMessage(windowsShortcut.description)
+                        tempList.add(windowsShortcut)
                     }
+                    if(sectionHeadingWindowsMap.get(getMessage(windowsShortcuts.key))){
+                        List existingKeys = sectionHeadingWindowsMap.get(getMessage(windowsShortcuts.key)) as List
+                        existingKeys.addAll(tempList)
+                        tempList = existingKeys
+                    }
+                    sectionHeadingWindowsMap.put(getMessage(windowsShortcuts.key), tempList)
+                }
+
+            }
         }
         shortcutKeys.mac.each { additionalMacList ->
             additionalMacList.each {sectionMacHeading ->
-                    List tempList
-                    sectionMacHeading.each { macShortcuts ->
-                        tempList = new ArrayList()
-                        macShortcuts.value.each { macShortcut ->
-                            macShortcut.combination = getMessage(macShortcut.combination)
-                            macShortcut.description = getMessage(macShortcut.description)
-                            tempList.add(macShortcut)
-                        }
-                        sectionHeadingMacMap.put(getMessage(macShortcuts.key), tempList)
+                List tempList
+                sectionMacHeading.each { macShortcuts ->
+                    tempList = new ArrayList()
+                    macShortcuts.value.each { macShortcut ->
+                        macShortcut.combination = getMessage(macShortcut.combination)
+                        macShortcut.description = getMessage(macShortcut.description)
+                        tempList.add(macShortcut)
                     }
+                    if(sectionHeadingMacMap.get(getMessage(macShortcuts.key))){
+                        List existingKeys = sectionHeadingMacMap.get(getMessage(macShortcuts.key)) as List
+                        existingKeys.addAll(tempList)
+                        tempList = existingKeys
+                    }
+                    sectionHeadingMacMap.put(getMessage(macShortcuts.key), tempList)
+                }
             }
         }
         outputJson {
