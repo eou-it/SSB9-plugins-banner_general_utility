9f7f7409962b08c75cea572d2a58aaa2e226171c
Ankur Jain <ankur.jain@ellucian.com>
2019-01-03 15:30:34 +0530

    Removing unnecessary default mapping - HRU-8280

--- grails-app/controllers/banner/general/utility/UrlMappings.groovy
+++ grails-app/controllers/banner/general/utility/UrlMappings.groovy
@@ -3,11 +3,6 @@ package banner.general.utility
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/"(view:"/index")
         "500"(view:'/error')
