bc6597e46af0fe3ffc1b2a8bb673aa13b383e3c9
Eshaan_kumar <eshaan.kumar@ellucian.com>
2019-01-22 09:16:29 +0530

    HRU-8464 Sanitize hideSSBHeaderComps in gsp

--- grails-app/controllers/SelfServiceMenuController.groovy
+++ grails-app/controllers/SelfServiceMenuController.groovy
@@ -79,9 +79,11 @@ class SelfServiceMenuController {
             if(session['hideSSBHeaderComps'] instanceof Boolean){
                 hideSSBHeader = session['hideSSBHeaderComps']
             } else{
-                def hideSSBHeaderValue = session['hideSSBHeaderComps'].trim()
+                def hideSSBHeaderValue = session['hideSSBHeaderComps']?.trim()
                 hideSSBHeader = Boolean.parseBoolean(hideSSBHeaderValue)
             }
+        }else{
+            session['hideSSBHeaderComps'] = hideSSBHeader
         }
         mnuList.eachWithIndex{ SelfServiceMenu,  i ->
             if(SelfServiceMenu.url.indexOf(MEPCODE)>-1 && session["mep"]!=null){
