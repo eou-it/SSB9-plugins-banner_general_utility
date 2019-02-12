2a9b614149e62e04ae93cbc68af4483cc50f9b35
Barun Kumar Patel <barun.patel@ellucian.com>
2019-02-12 09:01:09 +0530

    HRU-8462:-Added fix for Integration test.

--- src/integration-test/groovy/net/hedtech/banner/general/configuration/GeneralPageRoleMappingIntegrationTests.groovy
+++ src/integration-test/groovy/net/hedtech/banner/general/configuration/GeneralPageRoleMappingIntegrationTests.groovy
@@ -7,6 +7,7 @@ import grails.gorm.transactions.Rollback
 import grails.testing.mixin.integration.Integration
 import grails.util.Holders
 import net.hedtech.banner.testing.BaseIntegrationTestCase
+import static groovy.test.GroovyAssert.shouldFail
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
