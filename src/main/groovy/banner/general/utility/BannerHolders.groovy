f4bf7d0c55a379e2dc966e0078817a6b9cf579d2
Arun R Upadhya <arun.upadhya@ellucian.com>
2020-01-30 12:57:06 +0530

    HRU9230 adding comments and fixing review comments

--- src/main/groovy/banner/general/utility/BannerHolders.groovy
+++ src/main/groovy/banner/general/utility/BannerHolders.groovy
@@ -1,67 +1,77 @@
+/*******************************************************************************
+ Copyright 2020 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package banner.general.utility
 
 import grails.config.Config
-import grails.core.GrailsApplication
-import grails.plugins.GrailsPluginManager
-import grails.util.Holder
 import grails.util.Holders
 import groovy.transform.Synchronized
 import groovy.util.logging.Slf4j
-import org.apache.commons.logging.Log
-import org.apache.commons.logging.LogFactory
-import org.grails.core.io.support.GrailsFactoriesLoader
-import org.grails.core.support.GrailsApplicationDiscoveryStrategy
-import org.springframework.context.ApplicationContext
-import org.springframework.context.Lifecycle
-import org.springframework.util.Assert
 import org.springframework.web.context.request.RequestContextHolder
 
+/**
+ * Customized Holders class, and this singleton will used to support the MEP environment for SS-Configurations.
+ * This class is used in the BootStrap.groovy file to override 'Holders.config' which will internally calls the 'getConfig'.
+ */
 @Slf4j
 @Singleton
 public class BannerHolders {
 
-    public static Config getConfig() {
+    /**
+     * This static method is used to get Config object for MEP environment.
+     *
+     * @return Config   Config type of config object.
+     */
+    public static Config getConfig () {
         Config result = Holders.getGrailsApplication().config
+        // try and finally block, returning the config object in the finally block, we know that
+        // RequestContextHolder.currentRequestAttributes() method will throws an exception when the
+        // call to this block from BootStrap or Cron jobs etc., in this case it should return the non mep'd config object.
         try {
             String sessionMepCode = RequestContextHolder.currentRequestAttributes()?.request?.session?.getAttribute("mep")
-            setConfiguration(sessionMepCode, result)
+            setConfiguration( sessionMepCode, result )
         } finally {
+            // Returning the Config object.
             return result
         }
     }
 
     /**
-     * This method is used to set configuration
-     * @param mep
+     * This is the private synchronized static method and used to MEP the config object for MEP environment.
+     * If the MEP code is empty and config object is not null then it will process the config object and will replace the
+     * config properties with respect to MEP code.
+     *
+     * @param mep       String type of MEP code
+     * @param config    Config type of config object.
      */
     @Synchronized
     private static void setConfiguration(mep, config) {
         try {
-            if (mep != null && config != null) {
+            if ( mep != null && config != null ) {
                 final String mepKey = "_${mep}";
                 final ConfigSlurper configSlurper = new ConfigSlurper()
                 final Map<Object, Object> configMap = [:]
 
-                for (def entry : config.entrySet()) {
-                    configMap.put(entry.getKey(), config.get(entry.getKey()))
+                for ( def entry : config.entrySet() ) {
+                    configMap.put( entry.getKey(), config.get( entry.getKey() ) )
                 }
 
                 def foundMap = configMap.findAll { key, value ->
-                    key.toLowerCase().contains(mepKey.toLowerCase())
+                    key.toLowerCase().endsWith ( mepKey.toLowerCase() )
                 }
 
                 foundMap.each { foundKey, foundValue ->
                     configMap.each { key, value ->
-                        if (key.equals(foundKey.minus(mepKey))) {
+                        if ( key.equals( foundKey.minus( mepKey ) ) ) {
                             Properties propertyToMerge = new Properties()
-                            propertyToMerge.put(key, foundValue);
-                            config.merge(configSlurper.parse(propertyToMerge))
+                            propertyToMerge.put( key, foundValue )
+                            config.merge( configSlurper.parse( propertyToMerge ) )
                         }
                     }
                 }
             }
         } catch (Exception e) {
-            log.debug("Exception in BannerHolders.setConfiguration()", e.stackTrace);
+            log.debug( "Exception in BannerHolders.setConfiguration()", e.stackTrace );
         }
     }
 
