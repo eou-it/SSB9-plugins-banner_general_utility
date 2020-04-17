5e6efeb1965ebc4e189aac0985267b75c7a69b34
Arun Ramachandra Upadhya <Arun.Upadhya@ellucian.com>
2020-04-17 12:35:55 +0530

    HRU-9466 fix

--- src/main/groovy/banner/general/utility/BannerPropertySourcesConfig.groovy
+++ src/main/groovy/banner/general/utility/BannerPropertySourcesConfig.groovy
@@ -4,6 +4,7 @@
 package banner.general.utility
 
 import grails.config.Config
+import groovy.util.logging.Slf4j
 import org.grails.config.NavigableMap
 import org.grails.config.PropertySourcesConfig
 import org.springframework.web.context.request.RequestContextHolder
@@ -11,29 +12,39 @@ import org.springframework.web.context.request.RequestContextHolder
 /**
  * This class is extends the PropertySourcesConfig to customize for getting the "MEPed" configurations.
  */
+@Slf4j
 class BannerPropertySourcesConfig extends PropertySourcesConfig {
     @Override
     Object get(Object key) {
         final boolean isWebRequest = (RequestContextHolder.getRequestAttributes() != null)
         Object result = super.get(key)
-        if (isWebRequest) {
-            String sessionMepCode = RequestContextHolder.currentRequestAttributes()?.request?.session?.getAttribute("mep")
-            if (sessionMepCode) {
-                List<String> configList = super.get( "banner.mep.configurations" )
-                if ( configList && configList.get(0)?.toLowerCase() == 'all' ) {
-                    if (!(super.get("${sessionMepCode}.${key}") instanceof NavigableMap.NullSafeNavigator)) {
-                        result = super.get("${sessionMepCode}.${key}")
-                    }
-                } else if ( configList ) {
-                    Config configDB = BannerHolders.getMeppedConfigObjs().get('mepConfigList')
-                    if ( !(configDB."${key}" instanceof NavigableMap.NullSafeNavigator) ) {
+        try {
+            if (isWebRequest) {
+                String sessionMepCode = RequestContextHolder.currentRequestAttributes()?.request?.session?.getAttribute("mep")
+                if (sessionMepCode) {
+                    List<String> configList = super.get( "banner.mep.configurations" )
+                    if ( configList && configList.get(0)?.toLowerCase() == 'all' ) {
                         if (!(super.get("${sessionMepCode}.${key}") instanceof NavigableMap.NullSafeNavigator)) {
-                            result = super.get("${sessionMepCode}.${key}")
+                            if ( result instanceof NavigableMap && result.size() > 0 && ( result.size() != super.get("${sessionMepCode}.${key}").size() ) ) {
+                                result.merge( super.get("${sessionMepCode}.${key}") )
+                            } else {
+                                result = super.get("${sessionMepCode}.${key}")
+                            }
+                        }
+                    } else if ( configList ) {
+                        Config configDB = BannerHolders.getMeppedConfigObjs().get('mepConfigList')
+                        if ( !(configDB."${key}" instanceof NavigableMap.NullSafeNavigator) ) {
+                            if (!(super.get("${sessionMepCode}.${key}") instanceof NavigableMap.NullSafeNavigator)) {
+                                result = super.get("${sessionMepCode}.${key}")
+                            }
                         }
                     }
                 }
             }
+        } catch (e) {
+            log.error( "Error in BannerPropertySorucesConfig key = ${key}", e.stackTrace )
+        } finally {
+            return result
         }
-        return result
     }
 }
