1f9ff33fc6fd45fa1738eba0c2d75c686685bc5a
Arun Ramachandra Upadhya <Arun.Upadhya@ellucian.com>
2020-04-23 18:32:34 +0530

    HRU-9466 fixing the issues with boolean values

--- src/main/groovy/banner/general/utility/BannerPropertySourcesConfig.groovy
+++ src/main/groovy/banner/general/utility/BannerPropertySourcesConfig.groovy
@@ -21,43 +21,36 @@ class BannerPropertySourcesConfig extends PropertySourcesConfig {
         try {
             if (isWebRequest) {
                 String sessionMepCode = RequestContextHolder.currentRequestAttributes()?.request?.session?.getAttribute("mep")
-                if (sessionMepCode) {
+                if ( sessionMepCode && !(super.get( "banner.mep.configurations" ) instanceof NavigableMap.NullSafeNavigator) ) {
                     List<String> configList = super.get( "banner.mep.configurations" )
-                    if ( configList && configList.get(0)?.toLowerCase() == 'all' ) {
+                    if ( configList && configList.get(0)?.toLowerCase() == 'all' && !( configList instanceof NavigableMap.NullSafeNavigator ) ) {
                         if (!(super.get("${sessionMepCode}.${key}") instanceof NavigableMap.NullSafeNavigator)) {
-                            if ( result instanceof NavigableMap){
-                                //result.merge( super.get("${sessionMepCode}.${key}") )
-                                result =  getInnerPropertyConfigMap(sessionMepCode, result, key, key)
-                            } else {
-                                result = super.get("${sessionMepCode}.${key}")
-                            }
+                            result = getResultForMep( result, key, sessionMepCode )
                         }
-                    } else if ( configList ) {
+                    } else if ( configList && !( configList instanceof NavigableMap.NullSafeNavigator ) ) {
                         Config configDB = BannerHolders.getMeppedConfigObjs().get('mepConfigList')
                         if (!(super.get("${sessionMepCode}.${key}") instanceof NavigableMap.NullSafeNavigator)) {
                             if ( configDB.containsKey(key) ) {
-                                if ( result instanceof NavigableMap  ) {
-                                    configList.each { def mepKey ->
-                                        if ( configDB.containsProperty("${mepKey}") ) {
-                                            ConfigSlurper configSlurper = new ConfigSlurper()
-                                            Properties properties = new Properties()
-                                            properties.put("${mepKey}".substring("${mepKey}".indexOf('.') + 1), super.get("${sessionMepCode}.${mepKey}"))
-                                            result.merge( configSlurper.parse(properties) )
-                                        }
-                                    }
-                                } else {
-                                    result = super.get("${sessionMepCode}.${key}")
-                                }
+                                result = getResultForMep( result, key, sessionMepCode )
                             }
                         }
                     }
                 }
             }
         } catch (e) {
-            log.error( "Error in BannerPropertySorucesConfig key = ${key}", e.printStackTrace() )
+            log.error( "Error in BannerPropertySorucesConfig key = ${key}" )
         } finally {
             return result
         }
+    }
+
+    private Object getResultForMep ( result, key, sessionMepCode ) {
+        if ( result instanceof NavigableMap){
+            result =  getInnerPropertyConfigMap(sessionMepCode, result, key, key)
+        } else {
+            result = super.get("${sessionMepCode}.${key}")
+        }
+        return result
     }
 
     private def getInnerPropertyConfigMap(sessionMepCode, propertyConfigkey, parameterKey, parentkey){
@@ -70,7 +63,8 @@ class BannerPropertySourcesConfig extends PropertySourcesConfig {
                 innerPropertyMap << getInnerPropertyConfigMap(sessionMepCode, entryvalue, "${parameterKey}.${entrykey}", parentkey)
                 propertyMap.merge(innerPropertyMap)
             } else {
-                def configValue = super.get("${sessionMepCode}.${parameterKey}.${entrykey}") ?: grails.util.Holders.grailsApplication.config.get("${parameterKey}.${entrykey}")
+                def value = super.get("${sessionMepCode}.${parameterKey}.${entrykey}")
+                def configValue = !(value instanceof NavigableMap.NullSafeNavigator) ?: grails.util.Holders.grailsApplication.config.get("${parameterKey}.${entrykey}")
                 String configKey = "${parameterKey}.${entrykey}".minus("${parentkey}.")
                 property.put(configKey, configValue)
                 propertyMap = configSlurper.parse(property)
