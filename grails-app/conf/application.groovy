c0cb06148b5a2a1e9f05a304e9a4460a45369726
chirayuh <Chirayu.H@ellucian.com>
2020-07-20 16:48:25 +0530

    HRU-9671: Update copyright year.

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2009-2019 Ellucian Company L.P. and its affiliates.
+ Copyright 2009-2020 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 grails.project.groupId = "net.hedtech" // used when deploying to a maven repo
 
